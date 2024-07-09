dba6a150a918480db5a5c79ce1f67a619e19faab
Brook Tinnapat <53811329+BrookTinnapat@users.noreply.github.com>
2024-07-09 14:02:48 +0700

    feat: add my courses page

--- client/src/pages/authenticated-app.jsx
+++ client/src/pages/authenticated-app.jsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./non-authorized/loginpage";
 import Coursedetail from "./non-authorized/coursedetail";
 import HomePage from "./non-authorized/homepage";
 import LoginAdmin from "./admin/login-admin";
 import AddCourseAdmin from "./admin/addcourse-admin";
+import UserMycourse from "./authorized/user-mycourse";
 
 function AuthenticatedApp() {
   return (
@@ -12,8 +14,10 @@ function AuthenticatedApp() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/coursedetail" element={<Coursedetail />} />
-        <Route path="/login/admin" element={<LoginAdmin />} />
-        <Route path="/admin/addcourse/" element={<AddCourseAdmin />} />
+        <Route path="/admin/login" element={<LoginAdmin />} />
+        <Route path="/admin/addcourse" element={<AddCourseAdmin />} />
+        {/* Test path */}
+        <Route path="/user/my_course" element={<UserMycourse />} />
       </Routes>
     </div>
   );
