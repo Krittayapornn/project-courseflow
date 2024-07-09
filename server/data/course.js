9138eced50ccad23872afca1dc408b205fbf8240
Brook Tinnapat <53811329+BrookTinnapat@users.noreply.github.com>
2024-07-09 14:02:48 +0700

    feat: add my courses page

---
+++ server/data/course.js
@@ -0,0 +1,20 @@
+const courses = [
+  {
+    courseid: 1,
+    coursename: "Test1",
+    photo:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/1022px-Placeholder_view_vector.svg.png?20220519031949",
+    descripton:
+      "Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero. Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum.",
+  },
+  {
+    courseid: 2,
+    coursename: "Test2",
+    photo:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/1022px-Placeholder_view_vector.svg.png?20220519031949",
+    description:
+      "Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla.",
+  },
+];
+
+export default courses;
