6941352d7b8e4119a76287698ff184eb2e7728b5
Brook Tinnapat <53811329+BrookTinnapat@users.noreply.github.com>
2024-07-09 14:02:48 +0700

    feat: add my courses page

--- server/app.mjs
+++ server/app.mjs
@@ -4,7 +4,6 @@ import courseRouter from "./routes/course.mjs";
 import userRouter from "./routes/user.mjs";
 import connectionPool from "./utils/db.mjs";
 
-
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -30,6 +29,26 @@ app.get("/test", (req, res) => {
   return res.json("Server API is working 🚀");
 });
 
+app.get("/users", async (req, res) => {
+  let result;
+  try {
+    result = await client.query(`select * from users`);
+    res.status(200).json(result.rows);
+  } catch {
+    res.status(500).json({ message: `Internal Server Error` });
+  }
+});
+
+// Test get course
+app.get("/course", async (req, res) => {
+  let result;
+  try {
+    result = await client.query(`select * from courses`);
+    res.status(200).json(result.rows);
+  } catch {
+    res.status(500).json({ message: `Internal Server Error` });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running at ${port} 🚀`);
